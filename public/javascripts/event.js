13d79d30af3fd09637a5f50d4d48d698d321ea57
adslin <shihpei.ling@gmail.com>
2019-03-01 09:26:55 -0800

    Update event.js

--- public/javascripts/event.js
+++ public/javascripts/event.js
@@ -1,7 +1,7 @@
 document.addEventListener("click", changeColor);
 
 function changeColor() {  
-  if (document.getElementById("color").style.backgroundColor == "red"){
-      document.getElementById("color").style.backgroundColor = "blue"
- }else document.getElementById("color").style.backgroundColor = "red"
+  if (document.getElementById("color").style.backgroundColor == "blue"){
+      document.getElementById("color").style.backgroundColor = "red"
+ }else document.getElementById("color").style.backgroundColor = "blue"
 }
