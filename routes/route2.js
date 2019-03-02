75bc25506427facea7301eb51c2e5b2084dcfb03
adslin <shihpei.ling@gmail.com>
2019-03-02 06:54:10 -0800

    route2 - for all data

--- routes/route2.js
+++ routes/route2.js
@@ -1,29 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const colors = require('../colors');
+var data = require('../colors.json');
 
-router.get('/', (req, res) => {
-    	return res.send({ error: true, message: 'Colors App' })
+router.get('/all', function(req, res, next) {
+  res.send(data.colors);
 });
-
-router.get('/all', (req, res) => {
-     	var allColors = colors.getAll();
-     	var result = JSON.stringify(allColors, (error, result, fields) => {
-             if (error) {
-                res.status(500).send(error);
-            }
-                res.send(result);
-    });
-}); 
-
-router.get('/:color', (req, res) => {
-        var myColor = colors.getOne({color:req.params.color});
-        var result = JSON.stringify(myColor, (error, result, fields) => {
-             if (error) {
-                res.status(500).send(error);
-            }
-                res.send({ myColor: req.params.color }); 
-    });
-});  
 
 module.exports = router;
